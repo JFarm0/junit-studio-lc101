d2261142489ab34d2a6662dc6b31ea18e0b8440c
J Farmer <JFarm0>
2021-05-06 19:25:40 -0500

    Finished with Java Unit Testing Studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void onlyRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyBackwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void wordsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[words]"));
+    }
+
+    @Test
+    public void wordsBeforeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("words[]"));
+    }
+
+    @Test
+    public void wordsAfterBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]words"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void wordsWithOnlyLeftReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[words"));
+    }
+
+    @Test
+    public void wordsWithOnlyRightReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("words]"));
+    }
+
+    @Test
+    public void wordsWithBackwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]words["));
+    }
 }
